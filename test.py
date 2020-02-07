665dd6f793da26f3bc8eaeb7352d2baf6bafb57f
Sapienti17 <60652776+Sapienti17@users.noreply.github.com>
2020-02-07 14:13:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-a = "hello"
-dir(a)
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = input("Введите операцию:")
+
+if ((c == "/") or (c == "mod") or (c == "div")) and (b == 0):
+    print("Деление на 0!")
+elif c == "*":
+    print(a * b)
+elif c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "/":
+    print(a/b)
+elif c == "mod":
+    print(a%b)
+elif c == "pow":
+    print(a**b)
+elif c == "div":
+    print(a//b)
