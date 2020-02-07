275d17595ff34c6d0acd611f7f58f473a55329a5
Sapienti17 <60652776+Sapienti17@users.noreply.github.com>
2020-02-07 17:23:20 +0300

    master

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-c = input("Введите операцию:")
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# c = input("Введите операцию: ")
 
-if ((c == "/") or (c == "mod") or (c == "div")) and (b == 0):
-    print("Деление на 0!")
-elif c == "*":
-    print(a * b)
-elif c == "+":
-    print(a+b)
-elif c == "-":
-    print(a-b)
-elif c == "/":
-    print(a/b)
-elif c == "mod":
-    print(a%b)
-elif c == "pow":
-    print(a**b)
-elif c == "div":
-    print(a//b)
+# if ((c == "/") or (c == "mod") or (c == "div")) and (b == 0):
+#     print("Деление на 0!")
+# elif c == "*":
+#     print(a * b)
+# elif c == "+":
+#     print(a+b)
+# elif c == "-":
+#     print(a-b)
+# elif c == "/":
+#     print(a/b)
+# elif c == "mod" or "%":
+#     print(a%b)
+# elif c == "pow" or "**":
+#     print(a**b)
+# elif c == "div":
+#     print(a//b)
+
+age = 32
+name = 'Артур'
+print('Возраст {0} -- {1} лет.'.format(name, age))
+print('Почему {0} забавляется с этим Python?'.format(name))
